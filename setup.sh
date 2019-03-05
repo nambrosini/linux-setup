4e1629ba1abac3748d2e505cfb99054576c6b02e
Nico Ambrosini <nico.ambrosini@protonmail.com>
2019-03-05 15:00:02 +0100

    added some stuff

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-sudo pacman -Syu
-sudo pacman -S zsh thefuck base-devel
+sudo pacman -Syu -y
+sudo pacman -Rns gnome-terminal
+sudo pacman -S zsh thefuck base-devel vim exa hyperfine tilix yay
 
 # Zsh themes and plugins
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y
@@ -11,3 +12,11 @@ mv .zshrc ~/.zshrc
 mv .zsh_aliases ~/.zsh_aliases
 
 source ~/.zshrc
+
+# Fonts
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fura%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf?raw=true -P /usr/share/fonts
+
+# Awesome vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo 'set number' >> ~/.vim_runtime/vimrcs/basic.vim
