fd44ef9b2a0cc7d8009c658b0eeeba4c4034cf44
nambrosini <nambrosini@protonmail.com>
2020-06-04 21:58:11 +0200

    Modified to p10k

--- setup.sh
+++ setup.sh
@@ -1,19 +1,17 @@
-sudo pacman -Syu -y
-sudo pacman -S -y zsh thefuck base-devel vim exa hyperfine yay
+brew update
+brew install zsh thefuck vim exa
 
 # Zsh themes and plugins
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-mv .zshrc ~/.zshrc
-mv .zsh_aliases ~/.zsh_aliases
+cp .zshrc ~/.zshrc
+cp .zsh_aliases ~/.zsh_aliases
+cp .p10k.zsh ~/.p10k.zsh
 
 source ~/.zshrc
-
-# Fonts
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf -P /usr/share/fonts/Hack.ttf
 
 # Awesome vim
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
